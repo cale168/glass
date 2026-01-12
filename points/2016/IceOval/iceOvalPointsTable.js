cd2d48baa27c818db5c420703902fe0707b9ddb3
cale168 <cale_168@hotmail.com>
2026-01-12 18:13:50 -0500

    Create iceOvalPointsTable.js

---
+++ points/2016/IceOval/iceOvalPointsTable.js
@@ -0,0 +1,32 @@
+// Comparator function equivalent to PHP cmp()
+function cmp(a, b) {
+    let sumA = a[2] + a[3] + a[4];
+    let sumB = b[2] + b[3] + b[4];
+    return sumA > sumB ? -1 : 1; // Descending order
+}
+
+// Sort the array
+results.sort(cmp);
+
+let count = 0;
+results.forEach(team =>{
+	team
+	document.write("<tr><td>");
+	document.write(points[count+1]);
+	document.write("</td><td>");
+	document.write(count+1);
+	document.write("</td><td>");
+	document.write(team[0]);
+	document.write("</td><td>");
+	document.write(team[1]);
+	document.write("</td><td>");
+	document.write(team[2]);
+	document.write("</td><td>");
+	document.write(team[3]);
+	document.write("</td><td>");
+	document.write(team[4]);
+	document.write("</td><td>");
+	document.write(team[2]+team[3]+team[4]);
+	document.write("</td></tr>");
+	count = count +1 ;
+})
